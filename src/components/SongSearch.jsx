f896c0fdd88dd916b30080135e55533f2dd26231
Carlos Gámez <hardpx@gmail.com>
2024-08-23 20:56:48 -0600

    added fetch data from apis

--- src/components/SongSearch.jsx
+++ src/components/SongSearch.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react';
+import axios from 'axios';
 import SongForm from './SongForm';
 import SongDetails from './SongDEtails';
 import Loader from './Loader';
@@ -8,6 +9,44 @@ const SongSearcher = () => {
   const [lyrics, setLyrics] = useState(null);
   const [artist, setArtist] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    if (search === null) return;
+
+    const fetchData = async () => {
+      const { artist, song } = search;
+
+      let artistUrl = `https://www.theaudiodb.com/api/v1/json/2/search.php?s=${artist}`;
+      let songtUrl = `https://api.lyrics.ovh/v1/${artist}/${song}`;
+
+      const headers = {
+        'access-control-allow-origin': '*',
+        'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+        'Access-Control-Allow-Methods': '*',
+      };
+
+      const controller = new AbortController();
+
+      setIsLoading(true);
+
+      const [artistRes, songRes] = await Promise.all([
+        axios
+          .get(artistUrl, headers, { signal: controller.signal })
+          .then((res) => res.data)
+          .catch((err) => err.response),
+        axios
+          .get(songtUrl, { signal: controller.signal })
+          .then((res) => res.data)
+          .catch((err) => err.response),
+      ]);
+
+      setArtist(artistRes);
+      setLyrics(songRes);
+      setIsLoading(false);
+    };
+
+    fetchData();
+  }, [search]);
 
   const handleSearch = (data) => {
     setSearch(data);
